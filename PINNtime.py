eb4c372dcd029db8d8a359fe0ba5511d2d63a742
elNuNus <130293410+elNuNus@users.noreply.github.com>
2025-02-25 11:17:41 +0100

    Reworked the time prediction, again but it now trains !
    It turns out "inputs" was not very clear for a variable name, and x and t should be separated to simplify the code (and allow the computation of the time derivative).
    
    I use capital letters for tensors (X and T here)
    
    Also reformatted some variables names (number_ > n_) for readablity
    
    Thanks again to bonnatho for the codes

---
+++ PINNtime.py
@@ -0,0 +1,100 @@
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.hidden_layers = nn.ModuleList([nn.Linear(2, 20) if i == 0 else nn.Linear(20, 20) for i in range(8)])
+        self.output_layer = nn.Linear(20, 1)
+
+    def forward(self, x, t):
+        y = torch.stack([x,t],dim=-1)
+        print("y shape :",y.shape)
+        for layer in self.hidden_layers:
+            y = torch.tanh(layer(y))
+        out = self.output_layer(y)
+        # print("out shape :",out.shape)
+        return out
+
+def open_boundary(N, g_d_1=1, g_d_2=0):
+    t = torch.rand(N, dtype=torch.float32)
+    x = torch.cat([torch.ones(N // 2) * 1, torch.ones(N // 2) * -1], dim=0)
+    u = torch.cat([torch.ones(N // 2) * g_d_1, torch.ones(N // 2) * g_d_2], dim=0)
+    return x,t, u
+
+def init_cond(N):
+    x = torch.rand(N, dtype=torch.float32)*2-1
+    t = torch.zeros_like(x)
+    u = - torch.sin(5*np.pi * x)
+    return x, t, u
+
+def f(u, x, t, a):
+    u_x = torch.autograd.grad(u, x, grad_outputs=torch.ones_like(u), create_graph=True)[0]
+    u_xx = torch.autograd.grad(u_x, x, grad_outputs=torch.ones_like(u_x), create_graph=True)[0]
+    u_t = torch.autograd.grad(u, t, grad_outputs=torch.ones_like(u), create_graph=True)[0]
+    return u_t-a * u_xx
+
+N=128
+grids_xt = np.meshgrid(np.linspace(-1, 1, N), np.linspace(0, 1, 33), indexing='ij')
+grid_x, grid_t = [torch.tensor(t,dtype=torch.float32) for t in grids_xt]
+
+model = Network()
+
+u_pred_bc = model(grid_x,grid_t)
+
+
+N_bc = 100
+
+x_bc, t_bc, u_bc = [torch.stack([v_t0, v_x], dim=0) for v_t0, v_x in zip(init_cond(N_bc), open_boundary(N_bc))]
+#x_bc, t_bc, u_bc = np.asarray(x_bc,dtype=np.float32), np.asarray(t_bc,dtype=np.float32) ,np.asarray(u_bc,dtype=np.float32)
+
+
+N_in = 1000
+x_ph, t_ph = torch.rand(N_in)*2-1,torch.rand(N_in)
+x_ph.requires_grad = True
+t_ph.requires_grad = True
+
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+#optimizer = optim.Adam(model.parameters(), lr=0.001)
+ITERS = 500
+loss_plot = []
+for step in range(ITERS + 1):
+    optimizer.zero_grad()
+
+    # Boundary loss
+    u_pred_bc = model(x_bc,t_bc)
+    # print(u_pred_bc.squeeze(-1))
+    # print(u_bc)
+    loss_u = torch.nn.functional.mse_loss(u_pred_bc.squeeze(-1), u_bc)
+
+
+    # Physics loss
+    u_pred_ph = model(x_ph,t_ph)
+    #loss_ph = mse_loss(f(u_pred_ph, x_ph, a, g), torch.zeros_like(g)).mean()
+    loss_ph = torch.nn.functional.mse_loss(f(u_pred_ph,x_ph,t_ph,0.1),torch.zeros(N_in))
+    # Total loss
+    loss = loss_u +  loss_ph
+    loss.backward()
+    optimizer.step()
+
+    loss_plot.append(loss.item())
+    if step < 11 or step % 100 == 0:
+         print(f'Step {step}, loss: {loss.item()}')
+
+# Plot loss
+plt.plot(loss_plot)
+plt.yscale('log')
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.show()
+
+# Plot animated prediction
+u_pred = model(grid_x,grid_t).detach().numpy().reshape(N, 33)
+plt.imshow(u_pred, aspect='auto', extent=(-1, 1, 0, 1))
+plt.colorbar()
+plt.xlabel('x')
+plt.ylabel('t')
+plt.title('Predicted solution')
+plt.show()
